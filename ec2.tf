c7d7b768905c200733ebca4ddb35cb2a221f8d53
SahilGothoskar <gothoskarsahil@gmail.com>
2023-02-22 17:23:39 -0500

    Terraform Code

---
+++ ec2.tf
@@ -0,0 +1,154 @@
+provider "aws" {
+  region  = var.region
+  profile = "dev"
+}
+
+variable "region" {
+  type = string
+}
+variable "ami" {
+  type = string
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+
+resource "aws_subnet" "public" {
+  count = length(data.aws_availability_zones.available.names)
+
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "public-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(data.aws_availability_zones.available.names)
+
+  cidr_block        = "10.0.${count.index + 4}.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-${count.index + 1}"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(data.aws_availability_zones.available.names)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(data.aws_availability_zones.available.names)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_security_group" "instance" {
+  name_prefix = "instance-sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "all"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "Terraform_Managed" {
+  ami                         = var.ami
+  instance_type               = "t2.micro"
+  key_name                    = "SG"
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.instance.id]
+  associate_public_ip_address = true # enable public IP and DNS for the instance
+
+  tags = {
+    Name = "Terraform_Managed_Custom_AMI_Instance"
+  }
+
+  count = 1
+
+  lifecycle {
+    ignore_changes = [subnet_id]
+  }
+
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+#Note the addition of the count parameter to ensure that only one instance is created, as well as the use of the data.aws_availability_zones data source to retrieve the list of available availability zones for the selected region, and the use of the [0] index to ensure that the instance is launched in the first available zone. The lifecycle block with the ignore_changes parameter is also added to prevent changes to the subnet ID from triggering the creation of a new instance.
